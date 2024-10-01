02702a338feca95640788bebee4b7990c735df96
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-10-01 18:24:30 -0300

    initializing empty API project

---
+++ server/Br1InterviewPreparation.API/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Br1InterviewPreparation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "Test";
+        }
+    }
+}
