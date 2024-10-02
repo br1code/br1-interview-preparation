5671b16c096f014500ba16fd092df4667ec406ac
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-10-01 21:15:42 -0300

    adding endpoint for fetching all categories

--- server/Br1InterviewPreparation.API/Controllers/CategoriesController.cs
+++ server/Br1InterviewPreparation.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Br1InterviewPreparation.Application.Features.Categories.Dtos;
+using Br1InterviewPreparation.Application.Features.Categories.Queries.GetCategories;
 
 namespace Br1InterviewPreparation.API.Controllers
 {
@@ -6,10 +9,25 @@ namespace Br1InterviewPreparation.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        [HttpGet]
-        public string Test()
+        private readonly IMediator _mediator;
+
+        public CategoriesController(IMediator mediator)
         {
-            return "Test";
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Retrieves a list of categories.
+        /// </summary>
+        /// <returns>A list of categories.</returns>
+        /// <response code="200">Returns the list of categories</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CategoryDto>), 200)]
+        public async Task<IActionResult> GetCategories()
+        {
+            var query = new GetCategoriesQuery();
+            var categories = await _mediator.Send(query);
+            return Ok(categories);
         }
     }
 }
