cf8c4abada00815e51fd826709f7029f1a8f793e
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-10-02 10:02:03 -0300

    retrying db connection 5 times every 10 seconds

--- server/Br1InterviewPreparation.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ server/Br1InterviewPreparation.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -11,7 +11,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(connectionString));
+        {
+            options.UseNpgsql(connectionString, npgsqlOptions =>
+            {
+                npgsqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorCodesToAdd: null
+                );
+            });
+        });
 
         services.AddScoped<ICategoryRepository, CategoryRepository>();
 
