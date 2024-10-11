c38677da4a66fe880a39599a241cb4dc801d36cc
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-10-11 15:04:31 -0300

    adding basic CORS settings to start working on the frontend

--- server/Br1InterviewPreparation.API/Program.cs
+++ server/Br1InterviewPreparation.API/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Br1InterviewPreparation.API.Middlewares;
+using Br1InterviewPreparation.API.Settings;
 using Br1InterviewPreparation.Application.Extensions;
 using Br1InterviewPreparation.Infrastructure.Data;
 using Br1InterviewPreparation.Infrastructure.Extensions;
@@ -7,6 +8,8 @@ using Br1InterviewPreparation.Infrastructure.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<CorsSettings>(builder.Configuration.GetSection("CorsSettings"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,6 +23,17 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policyBuilder =>
+    {
+        var allowedOrigins = builder.Configuration.GetSection("CorsSettings:AllowedOrigins").Get<string[]>();
+        policyBuilder.WithOrigins(allowedOrigins)
+                     .AllowAnyHeader()
+                     .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -35,6 +49,8 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthorization();
 
---
+++ server/Br1InterviewPreparation.API/Settings/CorsSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Br1InterviewPreparation.API.Settings;
+
+public class CorsSettings
+{
+    public string[] AllowedOrigins { get; set; } = [];
+}
